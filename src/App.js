3e53d8e916f3abf8950e1cf76e11254dbfa248fd
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-05-08 03:08:15 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import SearchBar from './SearchBar';
-import Document from './Document';
+import SearchBar from './SearchBar.js';
+import Document from './Document.js';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
